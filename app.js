e525f62f10026534ce91c10f863678a53b9fdde3
candrews6 <charlie.andrews6@mail.com>
2024-01-20 21:40:46 +0000

    add floofs page, invert cursor, center header

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
+var floofsRouter = require('./routes/floofs');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
+app.use('/floofs', floofsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
