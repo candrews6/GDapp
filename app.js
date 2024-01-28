b6ee8c3d66c9231721c21b2d9ee06d9f11639a14
candrews6 <charlie.andrews6@mail.com>
2024-01-28 20:41:23 +0000

    install tailwind add command data aos logo

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
 var floofsRouter = require('./routes/floofs');
+var logoRouter = require('./routes/logo');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'src')))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,6 +38,8 @@ app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
 app.use('/floofs', floofsRouter);
+app.use('/logo', logoRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
